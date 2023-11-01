1c6bbe9b76dd68f08c7b371d972d568a2d1d5d3e
fatima2419 <126586807+fatima2419@users.noreply.github.com>
2023-10-31 22:22:19 -0300

    updatee

--- src/ThemeContext/theme-context.js
+++ src/ThemeContext/theme-context.js
@@ -1,7 +1,6 @@
 import * as React from "react";
 export const ThemeContext = React.createContext(
-  // default values used by a Consumer when it does not have a
-  // matching Provider above it in the tree, useful for testing
+  
   {
     themeContext: {
       color: "blue",
